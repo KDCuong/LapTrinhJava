1f7b84a1e2dc88376d4a68355d183297e8489624
curlykillah <curlykillah@gmail.com>
2018-04-18 18:57:48 +0700

    UserDAO

---
+++ ProfessionalToeic/src/professionaltoeic/DAO/UserDAO.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package professionaltoeic.DAO;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import professionaltoeic.Model.User;
+
+
+
+/**
+ *
+ * @author curly
+ */
+public class UserDAO {
+    private DataProvider dp;
+
+    public UserDAO() throws ClassNotFoundException,SQLException{
+        dp = new DataProvider();
+    }
+    public List<User> getAllUser() throws ClassNotFoundException,SQLException{
+        List<User> userlist= new ArrayList<>();
+        String sql = "SELECT * from user";
+        ResultSet rs = dp.executeReader(sql);
+        while(rs.next()){
+            int id = rs.getInt("user_id");
+            String name = rs.getString("user_name");
+            String fullname = rs.getString("user_fullname");
+            String password = rs.getString("password");
+            int type =rs.getInt("type");
+            String email =rs.getString("email");
+            int flag = rs.getInt("flag");
+            String status ="";
+            if(flag==1)
+                status= "In use";
+            else 
+                status = "Delete";
+            User u = new User(id,name,fullname,password,type,email,status);
+            userlist.add(u);
+        }
+        rs.close();
+        dp.closeDB();
+       return userlist ; 
+    }
+    
+}
---
+++ ProfessionalToeic/src/professionaltoeic/Model/User.java
@@ -0,0 +1,91 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package professionaltoeic.Model;
+
+/**
+ *
+ * @author curly
+ */
+public class User {
+    private int id;
+    private String name;
+    private String fullname;
+    private String password;
+    private int type;
+    private String email;
+    private String flag;
+
+    public User(int id, String name, String fullname, String password, int type, String email, String flag) {
+        this.id = id;
+        this.name = name;
+        this.fullname = fullname;
+        this.password = password;
+        this.type = type;
+        this.email = email;
+        this.flag = flag;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+   
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFlag() {
+        return flag;
+    }
+
+    public void setFlag(String flag) {
+        this.flag = flag;
+    }
+
+    public User() {
+    }
+}
