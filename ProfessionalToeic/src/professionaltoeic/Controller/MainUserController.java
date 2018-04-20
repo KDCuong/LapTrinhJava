c3f04432a9d3ba57c466324263052f67ac06ca95
KDCuong <1451010022cuong@ou.edu.vn>
2018-04-20 07:29:20 +0700

    insert css > repair scene

--- ProfessionalToeic/src/professionaltoeic/Controller/MainUserController.java
+++ ProfessionalToeic/src/professionaltoeic/Controller/MainUserController.java
@@ -5,13 +5,19 @@
  */
 package professionaltoeic.Controller;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 import professionaltoeic.DAO.UserDAO;
 
 
@@ -23,7 +29,11 @@ import professionaltoeic.DAO.UserDAO;
 public class MainUserController implements Initializable {
 
     @FXML
-    private Button btnT;
+    private Button btnListen;
+    @FXML
+    private Button btnGramma;
+    @FXML
+    private Button btnRead;
     @FXML
     private Label lbname;
     @FXML
@@ -32,18 +42,45 @@ public class MainUserController implements Initializable {
     private LoginController lc;
     
     @FXML
-    private void ButtonT(ActionEvent event){
-        btnT.setText("lala");
-        System.out.println(UserDAO.getLoginUser().getName());
+    private void ButtonListening(ActionEvent event) throws IOException{
+       Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+                    stage.hide();
+                    stage.setResizable(false);
+                    Parent root = FXMLLoader.load(getClass().getResource("FXML/ListeningTest.fxml"));
+                    Scene scene = new Scene(root);
+                    stage.setTitle("Listening");
+                    stage.setScene(scene);
+                    stage.show();
     }
-    
+    @FXML
+    private void ButtonGramma(ActionEvent event) throws IOException{
+         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+                    stage.hide();
+                    stage.setResizable(false);
+                    Parent root = FXMLLoader.load(getClass().getResource("FXML/GrammaTest.fxml"));
+                    Scene scene = new Scene(root);
+                    stage.setTitle("Gramma");
+                    stage.setScene(scene);
+                    stage.show();
+    }
+    @FXML
+    private void ButtonReading(ActionEvent event) throws IOException{
+         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+                    stage.hide();
+                    stage.setResizable(false);
+                    Parent root = FXMLLoader.load(getClass().getResource("FXML/ReadingTest.fxml"));
+                    Scene scene = new Scene(root);
+                    stage.setTitle("Reading");
+                    stage.setScene(scene);
+                    stage.show();
+    }
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-        lbname.setText(UserDAO.getLoginUser().getFullname());
+        lbname.setText(UserDAO.getLoginUser().getName());
         lbpoint.setText(String.valueOf(UserDAO.getLoginUser().getPoint()));
     }    
     
